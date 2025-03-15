f427e56961ec29eceba6bf5dff93e93d93516ebe
jhair <jinfanzonquispe30@gmail.com>
2025-03-15 04:50:10 -0500

    update

--- custom-video.js
+++ custom-video.js
@@ -101,31 +101,4 @@ function getYouTubeThumbnail(url) {
   return `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
 }
 
-  unlayer.registerTool({
-    name: 'my_tool',
-    label: 'My Tool',
-    icon: 'fa-smile',
-    supportedDisplayModes: ['web', 'email'],
-    options: {},
-    values: {},
-    renderer: {
-      Viewer: unlayer.createViewer({
-        render(values) {
-          return '<div>I am a custom tool.</div>';
-        },
-      }),
-      exporters: {
-        web: function (values) {
-          return '<div>I am a custom tool.</div>';
-        },
-        email: function (values) {
-          return '<div>I am a custom tool.</div>';
-        },
-      },
-      head: {
-        css: function (values) { },
-        js: function (values) { },
-      },
-    },
-  });
-
+  
