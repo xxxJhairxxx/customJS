652a949cc18ff352c513b62bf863170e9ca2d436
jhair <jinfanzonquispe30@gmail.com>
2025-03-15 04:57:56 -0500

    update

--- my-tool.js
+++ my-tool.js
@@ -1,29 +1,30 @@
-unlayer.registerTool({
-    name: 'my_tool',
-    label: 'My Tool',
-    icon: 'fa-smile',
-    supportedDisplayModes: ['web', 'email'],
-    options: {},
-    values: {},
-    renderer: {
-      Viewer: unlayer.createViewer({
-        render(values) {
-          return '<div>I am a custom tool.</div>';
+window.addEventListener('load', function() {
+    unlayer.registerTool({
+      name: 'my_tool',
+      label: 'My Tool',
+      icon: 'fa-smile',
+      supportedDisplayModes: ['web', 'email'],
+      options: {},
+      values: {},
+      renderer: {
+        Viewer: unlayer.createViewer({
+          render(values) {
+            return '<div>My Tool Loaded</div>';
+          },
+        }),
+        exporters: {
+          web: function (values) {
+            return '<div>My Tool Exported</div>';
+          },
+          email: function (values) {
+            return '<div>My Tool Email Exported</div>';
+          },
         },
-      }),
-      exporters: {
-        web: function (values) {
-          return '<div>I am a custom tool.</div>';
-        },
-        email: function (values) {
-          return '<div>I am a custom tool.</div>';
+        head: {
+          css: function (values) {},
+          js: function (values) {},
         },
       },
-      head: {
-        css: function (values) { },
-        js: function (values) { },
-      },
-    },
+    });
+    console.log('my-tool.js registered');
   });
-
-  console.log('my-tool.js loaded');
