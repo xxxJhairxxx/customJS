7b76d9a62a5cb018e24b83b1c6fffafb5a00e0f1
Jhair/ <55719931+xxxJhairxxx@users.noreply.github.com>
2025-03-15 02:35:06 -0500

    Create pick1.js

---
+++ pick1.js
@@ -0,0 +1,42 @@
+setTimeout(() => { unlayer.registerTool({
+  name: 'my_tool',
+  label: 'My Tool',
+  icon: 'fa-smile',
+  supportedDisplayModes: ['web', 'email'],
+  options: {
+    default: {
+      title: null,
+    },
+    text: {
+      title: 'Text',
+      position: 1,
+      options: {
+        textColor: {
+          label: 'Color',
+          defaultValue: '#ff0000',
+          widget: 'my_color_picker', // custom property editor
+        },
+      },
+    },
+  },
+  values: {},
+  renderer: {
+    Viewer: unlayer.createViewer({
+      render(values) {
+        return `<div style="color: ${values.textColor};">I am a custom tool.</div>`;
+      },
+    }),
+    exporters: {
+      web: function (values) {
+        return `<div style="color: ${values.textColor};">I am a custom tool.</div>`;
+      },
+      email: function (values) {
+        return `<div style="color: ${values.textColor};">I am a custom tool.</div>`;
+      },
+    },
+    head: {
+      css: function (values) {},
+      js: function (values) {},
+    },
+  },
+}) }, 1000);
